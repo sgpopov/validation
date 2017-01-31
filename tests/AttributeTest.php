b6cf926b7bc196e7d82dfe70bf4139adea8246d4
Svilen Popov <svil4ok@gmail.com>
2017-01-31 14:44:55 +0200

    New: attribute tests

---
+++ tests/AttributeTest.php
@@ -0,0 +1,165 @@
+<?php
+
+use svil4ok\Validation\Attribute;
+use svil4ok\Validation\Contracts\Rule;
+use svil4ok\Validation\Rules\Max;
+use svil4ok\Validation\Rules\Min;
+use svil4ok\Validation\Rules\Required;
+
+class AttributeTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var array
+     */
+    protected $rules = [];
+
+    /**
+     * Mock few rules before each test.
+     */
+    protected function setUp()
+    {
+        $minRule = $this->getMockBuilder(Min::class)
+            ->setMethods(['getSlug'])
+            ->getMock();
+
+        $minRule->expects($this->once())
+            ->method('getSlug')
+            ->with()
+            ->willReturn('min');
+
+        $maxRule = $this->getMockBuilder(Max::class)
+            ->setMethods(['getSlug'])
+            ->getMock();
+
+        $maxRule->expects($this->once())
+            ->method('getSlug')
+            ->with()
+            ->willReturn('max');
+
+        $requiredRule = $this->getMockBuilder(Required::class)
+            ->setMethods(['getSlug'])
+            ->getMock();
+
+        $requiredRule->expects($this->once())
+            ->method('getSlug')
+            ->with()
+            ->willReturn('required');
+
+        $this->rules = [$minRule, $maxRule, $requiredRule];
+    }
+
+    /**
+     * @test
+     *
+     * @covers Attribute
+     * @covers Attribute::addRule()
+     * @covers Attribute::getRule()
+     */
+    public function shouldRegisterRules()
+    {
+        $attributeName = 'username';
+        $required = false;
+
+        $attribute = new Attribute($attributeName, $this->rules, $required);
+
+        $this->assertTrue(is_array($attribute->getRules()));
+        $this->assertSame(3, count($attribute->getRules()));
+
+        array_map(function ($rule) {
+            $this->assertInstanceOf(Rule::class, $rule);
+        }, $attribute->getRules());
+    }
+
+    /**
+     * @test
+     *
+     * @covers Attribute
+     * @covers Attribute::addRule()
+     * @covers Attribute::isRequired()
+     */
+    public function attributeValidationShouldBeOptional()
+    {
+        $attributeName = 'username';
+        $required = false;
+
+        $attribute = new Attribute($attributeName, $this->rules, $required);
+
+        $this->assertFalse($attribute->isRequired());
+    }
+
+    /**
+     * @test
+     *
+     * @covers Attribute
+     * @covers Attribute::addRule()
+     * @covers Attribute::isRequired()
+     */
+    public function attributeValidationShouldBeRequired()
+    {
+        $attributeName = 'username';
+        $required = true;
+
+        $attribute = new Attribute($attributeName, $this->rules, $required);
+
+        $this->assertTrue($attribute->isRequired());
+    }
+
+    /**
+     * @test
+     *
+     * @covers Attribute
+     * @covers Attribute::addRule()
+     * @covers Attribute::getRule()
+     */
+    public function shouldReturnRuleIfRegistered()
+    {
+        $attributeName = 'username';
+        $required = true;
+
+        $attribute = new Attribute($attributeName, $this->rules, $required);
+
+        $this->assertNotNull($attribute->getRule('min'));
+        $this->assertNotNull($attribute->getRule('max'));
+        $this->assertNotNull($attribute->getRule('required'));
+
+        $this->assertNull($attribute->getRule('non-existent'));
+    }
+
+    /**
+     * @test
+     *
+     * @covers Attribute
+     * @covers Attribute::addRule()
+     * @covers Attribute::hasRule()
+     */
+    public function shouldDetermineIfRuleExists()
+    {
+        $attributeName = 'username';
+        $required = true;
+
+        $attribute = new Attribute($attributeName, $this->rules, $required);
+
+        $this->assertTrue($attribute->hasRule('min'));
+        $this->assertTrue($attribute->hasRule('max'));
+        $this->assertTrue($attribute->hasRule('required'));
+
+        $this->assertFalse($attribute->hasRule('non-existent'));
+    }
+
+    /**
+     * @test
+     *
+     * @covers Attribute
+     * @covers Attribute::addRule()
+     * @covers Attribute::getKey()
+     */
+    public function shouldReturnAttributeName()
+    {
+        $attributeName = 'username';
+        $required = true;
+
+        $attribute = new Attribute($attributeName, $this->rules, $required);
+
+        $this->assertSame($attributeName, $attribute->getKey());
+    }
+}

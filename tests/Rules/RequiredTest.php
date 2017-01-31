d0a2eafb56db46043204d2efffc524e445f35bfb
Svilen Popov <svil4ok@gmail.com>
2017-01-31 17:12:00 +0200

    New: required rule tests

---
+++ tests/Rules/RequiredTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Rules;
+
+use svil4ok\Validation\Rules\Required;
+
+class RequiredTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Required
+     */
+    protected $rule;
+
+    protected function setUp()
+    {
+        $this->rule = new Required();
+    }
+
+    /**
+     * @test
+     *
+     * @covers Required::passes()
+     */
+    public function shouldValidateNumericValues()
+    {
+        $this->assertTrue($this->rule->passes(10));
+        $this->assertTrue($this->rule->passes('10'));
+        $this->assertTrue($this->rule->passes('0'));
+        $this->assertTrue($this->rule->passes(0));
+        $this->assertTrue($this->rule->passes(-1));
+    }
+
+    /**
+     * @test
+     *
+     * @covers Required::passes()
+     */
+    public function shouldValidateStrings()
+    {
+        $this->assertTrue($this->rule->passes('string'));
+        $this->assertTrue($this->rule->passes(' string '));
+
+        $this->assertFalse($this->rule->passes(''));
+        $this->assertFalse($this->rule->passes('            '));
+    }
+
+    /**
+     * @test
+     *
+     * @covers Required::passes()
+     */
+    public function shouldValidateArray()
+    {
+        $this->assertTrue($this->rule->passes([1]));
+        $this->assertTrue($this->rule->passes([null]));
+        $this->assertTrue($this->rule->passes([null, null]));
+        $this->assertTrue($this->rule->passes(['string']));
+        $this->assertTrue($this->rule->passes([
+            'key' => 'value'
+        ]));
+
+        $this->assertFalse($this->rule->passes([]));
+    }
+
+    /**
+     * @test
+     *
+     * @covers Required::passes()
+     */
+    public function shouldValidateNull()
+    {
+        $this->assertFalse($this->rule->passes(null));
+
+        $this->assertTrue($this->rule->passes('null'));
+        $this->assertTrue($this->rule->passes(0));
+    }
+}
